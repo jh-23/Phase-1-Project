59344213d37b518fc85b27066e1273b9f4927d11
John Heger <heger7@gmail.com>
2023-11-13 17:18:00 -0600

    Added some html and uploaded info to DOM

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-document.addEventListener('DOMContentLoaded', () => console.log("we are connected!!!"))
+document.addEventListener('DOMContentLoaded', () => {
+
+    fetch('https://api.openbrewerydb.org/v1/breweries')
+        .then((response) => response.json())
+        .then((data) => data.forEach(breweryInfo => renderBreweryInformation(breweryInfo)))
+
+    function renderBreweryInformation(breweryInfo) {
+        console.log(breweryInfo)
+        const container = document.querySelector('#brewery-info-container')
+        const li = document.createElement('li')
+        li.textContent = breweryInfo.name
+        const a = document.createElement('a')
+        a.textContent = breweryInfo.website_url
+        a.href = breweryInfo.website_url
+        const p = document.createElement('p')
+        p.textContent = `${breweryInfo.city}, ${breweryInfo.state}        type: ${breweryInfo.brewery_type}`
+        container.append(li, a, p)
+    }
+
+
+
+
+
+
+
+})
