a6d78c9a43ecc1b3adec9f8d838affabdf097807
John Heger <heger7@gmail.com>
2023-11-14 08:38:23 -0600

    added city search filter, but still need to display on the DOM, works in the console

--- index.js
+++ index.js
@@ -13,10 +13,25 @@ document.addEventListener('DOMContentLoaded', () => {
         a.textContent = breweryInfo.website_url
         a.href = breweryInfo.website_url
         const p = document.createElement('p')
-        p.textContent = `${breweryInfo.city}, ${breweryInfo.state}        type: ${breweryInfo.brewery_type}`
+        p.textContent = `${breweryInfo.city}, ${breweryInfo.state}       / type: ${breweryInfo.brewery_type}`
         container.append(li, a, p)
     }
 
+    const form = document.querySelector('#brewery-city-search-form')
+    form.addEventListener('submit', searchCityBrewery)
+
+    function searchCityBrewery(event) {
+        event.preventDefault();
+        const searchCity = document.querySelector('#search')
+        console.log(searchCity.value)
+        fetch(`https://api.openbrewerydb.org/v1/breweries?by_city=${searchCity.value}`)
+            .then(response => response.json())
+            .then(data => data.forEach(brewerySearch => {
+                console.log(brewerySearch)
+                
+            }))
+
+    }
 
 
 
